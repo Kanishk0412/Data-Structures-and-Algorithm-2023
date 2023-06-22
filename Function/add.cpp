8899185622d4840f412a8222b4de0548b399eb61
Kanishk0412 <kanishktyagi200@gmail.com>
2023-06-23 01:04:07 +0530

    Function

---
+++ Function/add.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int add(int a, int b){
+    int sum = a + b;
+    return sum;
+}
+int main()
+{
+    int num1;
+    int num2;
+
+    cin>>num1>>num2;
+
+    cout<<add(num1,num2)<<endl;
+    return 0;
+}
---
+++ Function/fibenacci.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+    
+
+void fib(int n){
+    int t1=0;
+    int t2=1;
+    for (int i = 1; i <= n; i++)
+    {
+        cout<<t1<<endl;
+        int nextTerm=t1+t2;
+        t1=t2;
+        t2=nextTerm;
+    }
+    
+    
+}
+int main()
+{
+    int a;
+    cin>>a;
+
+    fib(a);
+    return 0;
+}
